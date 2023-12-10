c521e88d48c75c0404da512a6a01d1b96910c0bf
bruno gonzaga santos <brunog86@gmail.com>
2023-12-10 00:14:25 -0300

    Versão funcional do desafio

--- node/index.js
+++ node/index.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
+// MySQL connection
+const connection = mysql.createConnection({
+  host: 'db', // same as your MySQL service name in docker-compose
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+});
+
+connection.connect();
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  // Add a record
+  const sqlInsert = "INSERT INTO people(name) VALUES('Bruno')";
+  connection.query(sqlInsert, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+
+  // Fetch records
+  connection.query('SELECT name FROM people', function (error, results, fields) {
+    if (error) throw error;
+    res.send(`<h1>Full Cycle Rocks!</h1><ul>${results.map(row => `<li>${row.name}</li>`).join('')}</ul>`);
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
